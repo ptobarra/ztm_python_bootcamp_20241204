bf847127b15ecc44eae4270e367beb3a960f7bfe
ptobarra <12190195+ptobarra@users.noreply.github.com>
2025-03-30 18:40:51 +0200

    up o automation-testing /'selenium basics 2'

--- automation_testing/automation.py
+++ automation_testing/automation.py
@@ -1,45 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 chrome_browser = webdriver.Chrome()
 
 chrome_browser.maximize_window()
+chrome_browser.get('https://omayo.blogspot.com/') # notice the URL has changed from the video to the new demo site!
+
+print('omayo (QAFox.com)' in chrome_browser.title) # this should return True
+print('Python Easy Demo' in chrome_browser.title) # this should return False
+
+assert 'omayo (QAFox.com)' in chrome_browser.title # this should not raise an exception
+# assert 'Python Easy Demo' in chrome_browser.title # this should raise an exception
+
+# assert 'omayo (QAFox.com)' in chrome_browser.body # this should not raise an exception
+# assert 'Python Easy Demo' in chrome_browser.body # this should not raise an exception
+
+# find the element by class name
+# button = chrome_browser.find_element(By.CLASS_NAME, 'widget-content') # this should return a single element
+button = chrome_browser.find_element(By.ID, "but2") # this should return a single element
+# print(button)
+print(button.get_attribute('innerHTML')) # this should return the innerHTML of the element
 
 # i want selenium not to close the browser after the script is done
 input("press enter to close the browser...")
 chrome_browser.quit()
 
-
-
-
-
-
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# import time
-
-# #The below 3 lines ignores the https://demo.seleniumeasy.com SSL cert in case it is missing. Making your output cleaner. It isn't necessary, but added it here for you just in case. 
-# options = webdriver.ChromeOptions()
-# options.add_argument('--ignore-ssl-errors=yes')
-# options.add_argument('--ignore-certificate-errors')
-
-
-# chrome_browser = webdriver.Chrome(options=options)
-# chrome_browser.maximize_window()
-# chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html') # notice the URL has changed from the video to the new demo site!
-
-# # This solves the issue with the Popup for those that encounter it:
-# chrome_browser.implicitly_wait(2)
-# popup =chrome_browser.find_element(By.ID, 'at-cv-lightbox-close')
-# popup.click()
-
-
-
-# user_message = chrome_browser.find_element(By.ID, 'user-message')
-# user_message.clear()
-# user_message.send_keys('I AM EXTRA COOOOL')
-
-# time.sleep(2)
-# show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
-# show_message_button.click()
-
-# output_message = chrome_browser.find_element(By.ID, 'display')
-# assert 'I AM EXTRA COOOOL' in output_message.text
