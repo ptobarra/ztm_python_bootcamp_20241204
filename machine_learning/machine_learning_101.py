00f27b84b1aed4fa7ece354a4e537de9e05068b0
ptobarra <12190195+ptobarra@users.noreply.github.com>
2025-04-06 01:35:02 +0200

    up to machine learning/tools in machine learning

---
+++ machine_learning/machine_learning_101.py
@@ -0,0 +1,6 @@
+# 1- import the data (the hardest part is to get the data)
+# 2- clean the data
+# 3- split the data into training and testing sets
+# 4- create/train a model
+# 5- check the output with the test set
+# 6- improve the model
